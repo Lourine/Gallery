b27d9fe42172314b48be3babef49084c92de718f
Lourine <lourine.millicent@gmail.com>
2020-10-08 14:33:48 +0300

    Add mygallery as site root path

--- gallery/urls.py
+++ gallery/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
-   url(r'^mygallery/',include('mygallery.urls'))
+   url(r'',include('mygallery.urls'))
 ]
