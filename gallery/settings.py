d42db98aeef0c9f49c8fcf194e6ff1f36e2b1ec1
Lourine <lourine.millicent@gmail.com>
2020-10-12 10:03:12 +0300

    Change bootstrap 3 to bootstrap 4

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mygallery.apps.MygalleryConfig',
-    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
