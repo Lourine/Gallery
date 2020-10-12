c52ce4d5286266b99f2f04583b8f06cef173abae
Lourine <lourine.millicent@gmail.com>
2020-10-12 10:05:45 +0300

    Add category url to mygallery/urls.py

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^location/',views.location, name = 'location')
+    url(r'^location/(?P<location>\w+)/', views.location, name='location'),
+    url(r'^category/(?P<category>\w+)/', views.category, name='category'),
 ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
